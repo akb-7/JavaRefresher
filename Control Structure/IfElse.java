0d0ab3497658dd7927f102f6a1cd43875d8041b1
akb-7 <aakash.super77@gmail.com>
2022-05-22 17:28:18 +0530

    added input

--- Control Structure/IfElse.java
+++ Control Structure/IfElse.java
@@ -3,6 +3,11 @@ class IfElse {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(n);
+        if(n>5) {
+            System.out.println("The entered number is greater than 5");
+        }
+        else {
+            System.out.println("The entered number is lesser than or equal to 5");
+        }
     }
 }
